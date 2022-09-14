5fac42f87c575e56d5371a01b46e8821a02bd6a5
Siddharthan Dileep <79396917+vaasew@users.noreply.github.com>
2022-09-14 18:43:46 +0530

    Time: 194 ms (24.92%), Space: 48.2 MB (99.67%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int last=n-1;
+        
+        for(int i=n-2;i>=0;i--)
+         {
+             if(nums[i]+i>=last)
+             {
+                 last=i;
+             }
+            
+         }
+        if(last==0)
+        {return 1;}
+        else
+        {return 0;}
+    }
+ 
+};
