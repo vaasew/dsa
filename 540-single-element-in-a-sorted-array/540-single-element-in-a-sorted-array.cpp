c8841d21ce338b6fa2bc060bc14d427d9f09ffb8
Siddharthan Dileep <79396917+vaasew@users.noreply.github.com>
2022-09-19 20:07:04 +0530

    Time: 66 ms (13.12%), Space: 22.3 MB (55.70%) - LeetHub

---
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int start=0;
+        int end=nums.size()-1;
+        int mid;
+        while(start<=end)
+        {
+            mid=(start+end)/2;
+            if((mid!=nums.size()-1 and mid%2==0 and nums[mid]==nums[mid+1]) or (mid !=0 and mid%2==1 and nums[mid]==nums[mid-1]))
+            {
+                start=mid+1;
+                //we are at left side of single element
+            }
+            else if((mid !=0 and mid%2==0 and nums[mid]==nums[mid-1]) or (mid!=nums.size()-1 and mid%2==1 and nums[mid]==nums[mid+1]))
+            {
+            end=mid-1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return nums[mid];
+        
+    }
+};
