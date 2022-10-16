112b858cbdd5a8468a2b7283b551d305d20e6d54
Siddharthan Dileep <79396917+vaasew@users.noreply.github.com>
2022-10-16 09:21:01 +0530

    Time: 3 ms (54.11%), Space: 6.2 MB (67.10%) - LeetHub

---
+++ 796-rotate-string/796-rotate-string.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    
+    bool rotateString(string s, string goal) {
+        
+        
+        string s1=s+s;
+        
+        int m=goal.length();
+        int n=s1.length();
+        
+        if(m*2 != n)
+        {
+            return false;
+        }
+        
+        vector<int>lps= lpsCalc(m,goal);
+        
+        int i=0,j=0;
+        
+        while(i < n && j < m){
+            if(s1[i] == goal[j]){
+                i++;
+                j++;
+            }else{
+                if(j!=0)
+                {
+                    j = lps[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        if(j == m){
+            return true;
+        }
+        return false;
+        
+    }
+    
+    vector<int> lpsCalc(int m,string goal)
+    {
+        int j =0;
+        vector<int>lps(m);
+        
+        int i=1;
+        
+        while(i<m)
+        {
+            if(goal[i]==goal[j])
+            {
+                j++;
+                lps[i]=j;
+                i++;
+            }
+            else{
+                if(j!=0){
+                    j=lps[j-1];
+                }
+                else
+                {
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+};
